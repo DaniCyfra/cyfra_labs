49b45198e5d5f2f82a111ea2684cd499af1cf67f
Dani/cyfra <djcyfra@gmail.com>
2023-06-24 10:32:57 +0200

    ansible win

--- projects/000_scripts_proyects/windows/ansible_win_config.ps1
+++ projects/000_scripts_proyects/windows/ansible_win_config.ps1
@@ -6,17 +6,17 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 
 # Definir los detalles del usuario
 $usuario = "ansible"
-$contraseña = Read-Host -Prompt "Ingrese la contraseña"
+$pass = Read-Host -AsSecureString "Ingrese la password"
 
 # Crear el nuevo usuario
-$crearUsuario = New-LocalUser -Name $usuario -Password $contraseña -PasswordNeverExpires:$true -UserMayNotChangePassword:$true -AccountNeverExpires:$true -Description "Usuario para fines administrativos"
+$crearUsuario = New-LocalUser -Name $usuario -Password $pass -PasswordNeverExpires:$true -UserMayNotChangePassword:$true -AccountNeverExpires:$true -Description "Usuario para fines administrativos"
 
 # Asignar privilegios de administrador al usuario
 $grupoAdministradores = Get-LocalGroup -Name "Administradores"
 $grupoAdministradores | Add-LocalGroupMember -Member $usuario
 
 if ($crearUsuario) {
-    Write-Host "El usuario $usuario se ha creado con éxito y se ha agregado al grupo de administradores."
+    Write-Host "El usuario $usuario se ha creado con exito y se ha agregado al grupo de administradores."
 } else {
     Write-Host "Se produjo un error al crear el usuario."
 }
@@ -28,22 +28,22 @@ Start-Process -Wait -FilePath "python-installer.exe"
 Write-Host "Python se ha instalado correctamente."
 
 # Configurar la máquina de control de Ansible
-$ipUbuntuControl = Read-Host "Ingresa la dirección IP de la máquina de control de Ansible: "
+$ipUbuntuControl = Read-Host "Ingresa la direccion IP de la maquina de control de Ansible: "
 Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "$ipUbuntuControl" -Force
 
 # Solicitar la ruta del certificado SSL
-$rutaCertificadoSSL = Read-Host "Ingresa la ruta del certificado SSL: "
+#$rutaCertificadoSSL = Read-Host "Ingresa la ruta del certificado SSL: "
 
 # Ejecutar bloque problemático con CMD
-$cmdScript = @"
-@echo off
-echo Eliminando agente de escucha existente...
-powershell -Command "Remove-Item -Path WSMan:\localhost\Listener\Listener_443 -Recurse -Force"
-echo Agente de escucha eliminado correctamente.
-echo Creando nuevo agente de escucha...
-powershell -Command "New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -Thumbprint $(Get-PfxCertificate -FilePath '$rutaCertificadoSSL').Thumbprint -Force"
-echo Nuevo agente de escucha creado correctamente.
-"@
+#$cmdScript = @"
+#@echo off
+#echo Eliminando agente de escucha existente...
+#powershell -Command "Remove-Item -Path WSMan:\localhost\Listener\Listener_443 -Recurse -Force"
+#echo Agente de escucha eliminado correctamente.
+#echo Creando nuevo agente de escucha...
+#powershell -Command "New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -Thumbprint $(Get-PfxCertificate -FilePath '$rutaCertificadoSSL').Thumbprint -Force"
+#echo Nuevo agente de escucha creado correctamente.
+#"@
 
 $cmdScriptPath = "cmdScript.cmd"
 $cmdScript | Out-File -FilePath $cmdScriptPath -Encoding ASCII
@@ -57,4 +57,12 @@ $existingRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Silently
 if ($existingRule -eq $null) {
     try {
         New-NetFirewallRule -DisplayName $ruleName -Name "WinRM-HTTPS-In-TCP" -Protocol TCP -LocalPort 5986 -Action Allow
-        Write
+        Write-Host "Se ha creado la regla del Firewall correctamente."
+    } catch {
+        Write-Host "Error al crear la regla del Firewall: $_"
+        }
+    } else {
+        Write-Host "La regla del Firewall ya existe. No se ha creado una nueva regla."
+        }
+
+Write-Host "`n¡La configuración de WinRM se ha completado correctamente!"
