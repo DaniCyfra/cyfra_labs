f8289a31b51bd009b8110a3faf88fef328d8b971
DaniCyfra <djcyfra@gmail.com>
2024-09-21 12:40:05 +0200

    fixing errors

--- DAW/programacion/MiAplicacion/Program.cs
+++ DAW/programacion/MiAplicacion/Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-Console.WriteLine(“Hello World!”);
-Console.Write(“Programming...”);
+using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args, Console console)
+    {
+        console.Write(“Programming...”);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
