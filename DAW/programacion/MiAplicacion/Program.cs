c8bcfdf0eb0e34879359ad9eb17bb4f6c20dd2de
DaniCyfra <djcyfra@gmail.com>
2024-09-21 12:37:37 +0200

    Cambiado a nivel superior

--- DAW/programacion/MiAplicacion/Program.cs
+++ DAW/programacion/MiAplicacion/Program.cs
@@ -1,13 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
- namespace MiAplicación
- {
-   class Program
-   {
-       static void Main(string[] args)
-       {
-          Console.WriteLine(“Hello World!”);
-          Console.Write(“Programming...”);
-       }
-   }
- }
+Console.WriteLine(“Hello World!”);
+Console.Write(“Programming...”);
