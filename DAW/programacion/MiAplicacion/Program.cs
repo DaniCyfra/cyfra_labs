1de1291be5d769cbb77ea2d8ea602bc34e567d7a
DaniCyfra <djcyfra@gmail.com>
2024-09-21 12:30:15 +0200

    Creación proyecto miaplicacion

---
+++ DAW/programacion/MiAplicacion/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+ namespace MiAplicación
+ {
+   class Program
+   {
+       static void Main(string[] args)
+       {
+          Console.WriteLine(“Hello World!”);
+          Console.Write(“Programming...”);
+       }
+   }
+ }
